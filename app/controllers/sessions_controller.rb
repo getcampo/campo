de9b7b90e09a5c432c66ffca8607ac0c89f92f6b
Rei <chloerei@gmail.com>
2018-05-01 15:22:24 +0800

    Display login error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,11 +7,11 @@ class SessionsController < ApplicationController
   def create
     user = User.where("lower(username) = lower(:login) or lower(email) = lower(:login)", login: params[:login]).first
 
-    if user.authenticate(params[:password])
+    if user&.authenticate(params[:password])
       sign_in(user)
       redirect_to session[:return_path] || root_path
     else
-      redirect_to new_session_url, alert: 'Wrong login or password!'
+      redirect_to new_session_url, alert: 'Incorrect username or password.'
     end
   end
 
