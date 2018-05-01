c4938a8877e1537aece724599f761b2acb30a8d6
Rei <chloerei@gmail.com>
2018-05-01 20:02:06 +0800

    Remove unuse email auth

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,4 @@
 class UserMailer < ApplicationMailer
-  def auth_email(email)
-    @token = OmniAuth::Strategies::Email.token_for_email(email)
-
-    mail(to: email)
-  end
-
   def password_reset
     @user = params[:user]
     mail(subject: 'Reset password', to: @user.email)
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,4 @@
-require 'omniauth/strategies/email'
-
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :email
 end
 
 OmniAuth.config.logger = Rails.logger
