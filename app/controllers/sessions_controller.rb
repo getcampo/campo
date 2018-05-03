99c44c26bc74c7e0c1410be3ffb08cf19d75c8c6
Rei <chloerei@gmail.com>
2018-05-04 01:55:14 +0800

    render sign in error with form validate message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
       sign_in(user)
       redirect_to session[:return_path] || root_path
     else
-      redirect_to new_session_url, alert: 'Incorrect username or password.'
+      @sign_in_error = true
+      render
     end
   end
 
