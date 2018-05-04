67d40669f2fe76f9298a3c5d0a7c0e3d11d3d1ca
Rei <chloerei@gmail.com>
2018-05-04 20:28:33 +0800

    Add basic baord testing

--- test/controllers/boards_controller_test.rb
+++ test/controllers/boards_controller_test.rb
@@ -1,7 +1,37 @@
 require 'test_helper'
 
 class BoardsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get board index" do
+    get boards_url
+    assert_response :success
+  end
+
+  test "should get new pagae" do
+    sign_in_as users(:user)
+    get new_board_url
+    assert_response :success
+  end
+
+  test "should create board" do
+    sign_in_as users(:user)
+    assert_difference "Board.count" do
+      post boards_url, params: { board: { name: 'Foo', slug: 'foo', description: 'text' }}
+    end
+    assert_redirected_to board_url(Board.last)
+  end
+
+  test "should get edit page" do
+    sign_in_as users(:user)
+    board = Board.create(name: 'Foo', slug: 'foo', description: 'text')
+    get edit_board_url(board)
+    assert_response :success
+  end
+
+  test "should update board" do
+    sign_in_as users(:user)
+    board = Board.create(name: 'Foo', slug: 'foo', description: 'text')
+    patch board_url(board), params: { board: { name: 'Change' } }
+    assert_redirected_to board_url(board)
+    assert_equal 'Change', board.reload.name
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,9 +2,24 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+module SessionHelper
+  def sign_in_as(user)
+    cookies[:auth_token] = user.auth_token
+  end
+
+  def sign_out
+    cookies[:auth_token] = nil
+  end
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+
+
+class ActionDispatch::IntegrationTest
+  include SessionHelper
 end
