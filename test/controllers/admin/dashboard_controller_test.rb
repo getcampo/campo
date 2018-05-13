f487024e03c1dd3c39825dc1e673b6d36c8b6b29
Rei <chloerei@gmail.com>
2018-05-14 01:35:16 +0800

    Remove unuse test

--- test/controllers/admin/dashboard_controller_test.rb
+++ test/controllers/admin/dashboard_controller_test.rb
@@ -2,8 +2,8 @@ require 'test_helper'
 
 class Admin::DashboardControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get admin_dashboard_index_url
+    sign_in_as(users(:admin))
+    get admin_root_url
     assert_response :success
   end
-
 end
--- test/controllers/forums_controller_test.rb
+++ test/controllers/forums_controller_test.rb
@@ -5,33 +5,4 @@ class ForumsControllerTest < ActionDispatch::IntegrationTest
     get forums_url
     assert_response :success
   end
-
-  test "should get new pagae" do
-    sign_in_as users(:admin)
-    get new_forum_url
-    assert_response :success
-  end
-
-  test "should create forum" do
-    sign_in_as users(:admin)
-    assert_difference "Forum.count" do
-      post forums_url, params: { forum: { name: 'Foo', slug: 'foo', description: 'text' }}
-    end
-    assert_redirected_to forum_url(Forum.last)
-  end
-
-  test "should get edit page" do
-    sign_in_as users(:admin)
-    forum = Forum.create(name: 'Foo', slug: 'foo', description: 'text')
-    get edit_forum_url(forum)
-    assert_response :success
-  end
-
-  test "should update forum" do
-    sign_in_as users(:admin)
-    forum = Forum.create(name: 'Foo', slug: 'foo', description: 'text')
-    patch forum_url(forum), params: { forum: { name: 'Change' } }
-    assert_redirected_to forum_url(forum)
-    assert_equal 'Change', forum.reload.name
-  end
 end
