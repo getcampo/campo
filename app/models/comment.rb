c74d0eebae09a5d4b270feef5030f091fcb7877a
Rei <chloerei@gmail.com>
2018-06-02 17:44:16 +0800

    Add comment notification logic

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,4 +6,20 @@ class Comment < ApplicationRecord
   belongs_to :reply_to_comment, class_name: 'Comment', optional: true
 
   validates :content, presence: true
+
+  after_commit :create_comment_notifications, :create_reply_notifications, on: [:create]
+
+  private
+
+  def create_comment_notifications
+    if user != topic.user
+      topic.user.notifications.create(name: 'comment', source: self)
+    end
+  end
+
+  def create_reply_notifications
+    if reply_to_comment && user != reply_to_comment.user && reply_to_comment.user != topic.user
+      reply_to_comment.user.notifications.create(name: 'reply', source: self)
+    end
+  end
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should create comment notifications" do
+    topic = create(:topic)
+    assert_difference "topic.user.notifications.count" do
+      create(:comment, topic: topic)
+    end
+    notification = topic.user.notifications.last
+    assert_equal 'comment', notification.name
+    assert_equal Comment.last, notification.source
+  end
+
+  test "should create reply notifications" do
+    comment = create(:comment)
+    assert_difference "comment.user.notifications.count" do
+      create(:comment, reply_to_comment: comment)
+    end
+    notification = comment.user.notifications.last
+    assert_equal 'reply', notification.name
+    assert_equal Comment.last, notification.source
+  end
+
+  test "should not create duplicate reply notifications for topic user" do
+    topic = create(:topic)
+    comment = create(:comment, topic: topic, user: topic.user)
+    assert_difference "comment.user.notifications.count" do
+      create(:comment, topic: topic, reply_to_comment: comment)
+    end
+    notification = topic.user.notifications.last
+    assert_equal 'comment', notification.name
+    assert_equal Comment.last, notification.source
+  end
 end
