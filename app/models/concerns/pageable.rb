b048213aa5c4921bb67c0ca0e676d1dcc0bbc66d
Rei <chloerei@gmail.com>
2018-06-06 01:36:59 +0800

    Fix pageable offset

--- app/models/concerns/pageable.rb
+++ app/models/concerns/pageable.rb
@@ -2,6 +2,6 @@ module Pageable
   extend ActiveSupport::Concern
 
   included do
-    scope :page, -> (page) { offset(page.to_i * 25).limit(25) }
+    scope :page, -> (page) { offset(([page.to_i, 1].max - 1) * 25).limit(25) }
   end
 end
