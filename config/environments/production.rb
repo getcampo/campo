7d3574fcb8bf7d4c1fa63fd20f25ec4a9b714243
Rei <chloerei@gmail.com>
2018-06-23 23:27:45 +0800

    Add public file cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,9 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.headers = {
+    'Cache-Control' => "public, max-age=#{2.days.to_i}"
+  }
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
