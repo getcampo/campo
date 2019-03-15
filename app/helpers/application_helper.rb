535ec28a37505d963554aa4fa98535153a87104e
Rei <chloerei@gmail.com>
2019-03-15 20:53:06 +0800

    Edit topic index style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@ module ApplicationHelper
   def avatar_tag(user, options = {})
     size = options.fetch :size, '1x'
     if user.avatar.attached?
-      image_tag(url_for(user.avatar.variant(combine_options: { resize: '160x160^', gravity: 'center', extent: '160x160' })), class: "avatar avatar-#{size}")
+      image_tag(url_for(user.avatar.variant(combine_options: { resize: '160x160^', gravity: 'center', extent: '160x160' })), class: "avatar avatar-#{size} rounded")
     else
-      content_tag :div, user.name[0], class: "letter-avatar letter-avatar-#{size}"
+      content_tag :div, user.name[0], class: "avatar avatar-#{size} letter-avatar rounded"
     end
   end
 
