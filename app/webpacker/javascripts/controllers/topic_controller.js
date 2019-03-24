ffb3e31aa160aa65f25a2d23b8fd3c87d389b6d8
Rei <chloerei@gmail.com>
2019-03-24 15:45:55 +0800

    Use html template and client side logic to replace comments

--- app/webpacker/javascripts/controllers/topic_controller.js
+++ app/webpacker/javascripts/controllers/topic_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "stimulus"
 import Rails from "rails-ujs"
 
 export default class extends Controller {
-  static targets = ['slider']
+  static targets = ['slider', 'comments']
 
   connect() {
     this.sliderTarget.addEventListener('slider.change', this.visitPosition.bind(this))
@@ -15,7 +15,19 @@ export default class extends Controller {
     Rails.ajax({
       url: url.href,
       type: 'get',
-      dataType: 'script',
+      dataType: 'html',
+      success: (data) => {
+        this.commentsTarget.outerHTML = data.querySelector('#comments').outerHTML
+        this.focusComment()
+      }
     })
+  }
+
+  focusComment() {
+    if (this.commentsTarget.dataset.focusId) {
+      window.scrollTo(0, document.querySelector(`#comment-${this.commentsTarget.dataset.focusId}`).getBoundingClientRect().top + window.scrollY - 64)
+    } else {
+      window.scrollTo(0, 0)
+    }
   }
 }
