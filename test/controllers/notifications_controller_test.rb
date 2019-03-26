a35b3bfb379302abb4950fb8831c062b554218b7
Rei <chloerei@gmail.com>
2019-03-26 20:46:15 +0800

    Remove unused comment model

--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -1,12 +1,4 @@
 require 'test_helper'
 
 class NotificationsControllerTest < ActionDispatch::IntegrationTest
-  test "auto mark all as read" do
-    user = create(:user)
-    user.notifications.create(name: 'comment', record: create(:comment))
-    sign_in_as user
-    get notifications_url
-    assert_response :success
-    assert_equal 0, user.notifications.unread.count
-  end
 end
--- test/factories/comments.rb
+++
@@ -1,7 +0,0 @@
-FactoryBot.define do
-  factory :comment do
-    user
-    topic
-    content { "Content" }
-  end
-end
