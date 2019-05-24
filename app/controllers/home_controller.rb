1326dd7c6f2c110b81d89667ff8ebbb4a12b91f3
Rei <chloerei@gmail.com>
2019-05-24 17:09:46 +0800

    Move topics template

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,5 @@
 class HomeController < ApplicationController
   def index
     @topics = Topic.includes(:user).order(activated_at: :desc).page(params[:page])
-
-    render 'topics/index'
   end
 end
