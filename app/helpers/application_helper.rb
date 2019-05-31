8d7bb7db9a3ff1120c1ead4c83e18a61552a08cc
Rei <chloerei@gmail.com>
2019-05-31 20:03:47 +0800

    Remove avatar helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,4 @@
 module ApplicationHelper
-  def avatar_tag(user, options = {})
-    size = options.fetch :size, '1x'
-    image_tag(avatar_url(user), class: "avatar avatar-#{size} rounded")
-  end
-
-  def avatar_url(user)
-    user.avatar.thumb.url
-  end
-
   def local_time_or_date(time)
     local_relative_time time, type: 'time-or-date'
   end
