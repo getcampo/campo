0489766cf0a4e2abaf1b1b2c0f1e27608d33d963
Rei <chloerei@gmail.com>
2019-06-07 14:35:47 +0800

    Add reaction controller test

--- test/controllers/reactions_controller_test.rb
+++ test/controllers/reactions_controller_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class ReactionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = create(:user)
+    @post = create(:post)
+  end
+
+  test "should like post" do
+    sign_in_as @user
+    post post_reaction_path(@post, type: 'like'), xhr: true
+    assert_response :success
+    assert @user.reactions.where(post: @post).like.exists?
+  end
+
+  test "should dislike post" do
+    sign_in_as @user
+    post post_reaction_path(@post, type: 'dislike'), xhr: true
+    assert_response :success
+    assert @user.reactions.where(post: @post).dislike.exists?
+  end
+
+  test "should remove like post" do
+    sign_in_as @user
+    @user.reactions.create(post: @post, type: 'like')
+    delete post_reaction_path(@post, type: 'like'), xhr: true
+    assert_response :success
+    assert_not @user.reactions.where(post: @post).like.exists?
+  end
+
+  test "should remove dislike post" do
+    sign_in_as @user
+    @user.reactions.create(post: @post, type: 'dislike')
+    delete post_reaction_path(@post, type: 'dislike'), xhr: true
+    assert_response :success
+    assert_not @user.reactions.where(post: @post).dislike.exists?
+  end
+
+  test "should like/dislike mutually exclusive" do
+    sign_in_as @user
+    @user.reactions.create(post: @post, type: 'like')
+
+    post post_reaction_path(@post, type: 'dislike'), xhr: true
+    assert_response :success
+    assert_not @user.reactions.where(post: @post).like.exists?
+    assert @user.reactions.where(post: @post).dislike.exists?
+
+    post post_reaction_path(@post, type: 'like'), xhr: true
+    assert_response :success
+    assert @user.reactions.where(post: @post).like.exists?
+    assert_not @user.reactions.where(post: @post).dislike.exists?
+  end
 end
