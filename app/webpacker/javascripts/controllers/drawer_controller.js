af089765ce30100c744961d3996b02278c7d1318
Rei <chloerei@gmail.com>
2019-06-25 16:43:37 +0800

    Keep drawer open state

--- app/webpacker/javascripts/controllers/drawer_controller.js
+++ app/webpacker/javascripts/controllers/drawer_controller.js
@@ -1,22 +1,36 @@
 import { Controller } from "stimulus"
 
-export default class extends Controller {
+export default class DrawerController extends Controller {
+  static keepOpen = false
+
   connect() {
     let toggle = document.querySelector(`[data-toggle='#${this.element.id}']`)
     toggle.addEventListener('click', () => {
       this.toggle()
     })
+
+    if (DrawerController.keepOpen) {
+      this.open()
+    } else {
+      this.close()
+    }
+  }
+
+  disconnect() {
+    if (window.innerWidth > 1200) {
+      DrawerController.keepOpen = this.element.classList.contains('open')
+    }
   }
 
   toggle(event) {
     this.element.classList.toggle('open')
   }
 
+  open() {
+    this.element.classList.add('open')
+  }
+
   close() {
     this.element.classList.remove('open')
   }
 }
-
-document.addEventListener('turbolinks:before-cache', () => {
-  document.querySelector('.drawer.open').classList.remove('open')
-})
