b72ba4cbf1c42e65e5e2de5021e4ba65a1076e5d
Rei <chloerei@gmail.com>
2019-08-19 21:03:49 +0800

    touch post when create/delete reaction

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -2,7 +2,7 @@ class Reaction < ApplicationRecord
   self.inheritance_column = '_type_disabled'
 
   belongs_to :user
-  belongs_to :post
+  belongs_to :post, touch: true
 
   enum type: {
     like: 0,
