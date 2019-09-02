e6d779bf380c97fb8ac66686be5dcd7be12f9f8d
Rei <chloerei@gmail.com>
2019-09-02 20:10:25 +0800

    Remove unuse sprockets config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,15 +26,6 @@ Rails.application.configure do
     'Cache-Control' => "public, max-age=#{2.days.to_i}"
   }
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
-
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
-  # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
